ad8a9a37f23ff9ecdc4fabf9f605329cb349a0f0
Yuliia Kushnyrenko <kushnyrenkouliia8gmail.com>
2020-10-07 15:52:29 +0300

    Updates

---
+++ precache-manifest.5de07781b25b12e3de9a0ecae1e51462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f3f856d948b9a767d16e75e57668e5",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "cbd1a7dbb40d88b7bfd6",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.e4669bb6.chunk.css"
+  },
+  {
+    "revision": "ccbcb0f8e5bb64782f4b",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.f9e43cf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.f9e43cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd1a7dbb40d88b7bfd6",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.aa4587f9.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
